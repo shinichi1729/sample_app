5c3020c41316b648fcb22543d2c7da908da7c799
shinichi <shin@example.com>
2022-10-13 15:09:50 +0900

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup', to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   root "static_pages#home"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
