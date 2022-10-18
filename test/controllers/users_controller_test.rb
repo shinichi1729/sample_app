cd467d67f044e043f0d8ee6afb36fef39d567560
shinichi <shin@example.com>
2022-10-18 18:24:45 +0900

    Finish user edit, update, index, and destroy action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,6 +17,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should redirect update when not logged in" do
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
@@ -36,6 +41,31 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  # test "should not allow the admin attribute to be edited via the web" do
+  #   log_in_as(@other_user)
+  #   assert_not @other_user.admin?
+  #   patch user_path(@other_user), params: {
+  #                                   user: { password:              @other_user.password,
+  #                                           password_confirmation: @other_user.password,
+  #                                           admin: false } }
+  #   assert_not @other_user.toggle!(:admin).admin?
+  # end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to root_url
   end
 
